aa358c93ac992334020b478a099ef3e1053c439f
Jaya <jaya.wired@gmail.com>
2020-08-26 21:45:21 +1000

    databese migrated from mLab to atlas

--- db/index.js
+++ db/index.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
-
+let mongodbURI;
+   if(process.env.MONGODB_URI==undefined){
+        mongodbURI= 'mongodb+srv://admin:admin@cluster0.ppebl.mongodb.net/slotengine?retryWrites=true&w=majority'
+   }else{
+        mongodbURI =process.env.MONGODB_URI
+   }
 const connection = mongoose
-    .connect('mongodb://heroku_7crf5fw7:41fipvf9e51f8jduav5396vb1@ds161823.mlab.com:61823/heroku_7crf5fw7' , { useNewUrlParser: true ,useUnifiedTopology: true },)
-
+    .connect(mongodbURI, { useNewUrlParser: true ,useUnifiedTopology: true },)
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
  
 const db = mongoose.connection
-
 
 module.exports = db
